3c28019c237cea7cb7196ce23801e7ac26c00ddb
Tuddy09 <115088565+Tuddy09@users.noreply.github.com>
2024-04-16 16:19:26 +0300

    Removed web sockets, don't care no more

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 // App.js
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import MasterPage from './Components/MasterPage';
 import DetailPage from './Components/DetailPage';
 import CarContext from "./Components/CarContext";
-import {io} from "socket.io-client";
 import LoginDialog from "./Components/SignInPage";
 import User from "./Components/User";
 import UserContext from "./Components/UserContext";
@@ -54,22 +53,6 @@ function App() {
             alert('Invalid username or password');
         });
     };
-    useEffect(() => {
-        const socket = io('http://localhost:9092');
-        socket.on('connect_error', (error) => {
-            console.error('Connection error:', error);
-        });
-        socket.on('connect_timeout', () => {
-            console.error('Connection timeout');
-        });
-        socket.on('newCar', (newCar) => {
-            setData(prevData => [...prevData, newCar]);
-            console.log('New car added!', newCar)
-        });
-        return () => {
-            socket.disconnect();
-        };
-    }, [setData]);
 
     return (
         <UserContext.Provider value={{user, setUser}}>
